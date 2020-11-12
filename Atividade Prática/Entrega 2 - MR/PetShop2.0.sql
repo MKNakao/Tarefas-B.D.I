7afe8f6284945010f923b9ba6e73ebc4aff6ec10
KNakao <54083799+MKNakao@users.noreply.github.com>
2020-11-11 21:27:40 -0300

    Correções Finais

--- Atividade Prática/Entrega 2 - MR/PetShop2.0.sql
+++ Atividade Prática/Entrega 2 - MR/PetShop2.0.sql
@@ -18,21 +18,21 @@ drop table if exists Funcionario;
 
 Create table Funcionario(
 	CPF_F integer primary key,
-    Nome varchar(128) NOT NULL,
-    Salario_H decimal(10,2) NOT NULL
+    Nome varchar(128),
+    Salario_H decimal(10,2)
 );
 
 create table Empregado(
-	CPF_E integer,
-    HoraDTrabalho float NOT NULL,
-    Qtde_Funcoes integer NOT NULL,
-    Data_Inicio date NOT NULL,
+	CPF_E integer primary key,
+    HoraDTrabalho float,
+    Qtde_Funcoes integer,
+    Data_Inicio date,
     foreign key (CPF_E) references Funcionario(CPF_F)
 );
 
 create table Funcao(
 	Funct_ID integer primary key,
-    Nome varchar(128) NOT NULL,
+    Nome varchar(128),
     Descricao varchar(512)
 );
 
@@ -46,31 +46,31 @@ create table Empregado_tem_Funcao(
 
 create table Veiculo(
 	Placa char(7) primary key,
-    Modelo varchar(128) NOT NULL,
-    Manutencao decimal(10,2) NOT NULL
+    Modelo varchar(128),
+    Manutencao decimal(10,2)
 );
 
 create table Motorista(
-	CPF_M integer,
+	CPF_M integer primary key,
     Custo_Entrega decimal(10,2),
-    CNH char(11) NOT NULL,
-    Placa_veiculo char(7),
-    Telefone char(12) NOT NULL, /* (44 1234567) ou (44 123456789) */
+    CNH char(11),
+    Telefone char(12), /* (44 1234567) ou (44 123456789) */
+    Placa_veiculo char(7) NOT NULL,
     foreign key (CPF_M) references Funcionario(CPF_F),
     foreign key (Placa_veiculo) references Veiculo(Placa)
 );
 
 create table Servico(
 	Servico_ID integer primary key,
-    Nome_Servico varchar(256) NOT NULL,
-    Custo decimal(10,2) NOT NULL,
-    Tempo_Servico float NOT NULL
+    Nome_Servico varchar(256),
+    Custo decimal(10,2),
+    Tempo_Servico float
 );
 
 create table Produto(
 	Produto_ID integer primary key,
-    Nome varchar(256) NOT NULL,
-    Marca varchar(126) NOT NULL,
+    Nome varchar(256),
+    Marca varchar(126),
     Descricao varchar(256)
 );
 
@@ -92,23 +92,23 @@ create table Empregado_Faz_Servico(
 
 create table Raca(
 	Race_ID integer primary key,
-    Raca varchar(128),
+    Nome varchar(128),
     Especie varchar(128),
     Porte varchar(64) /*Grande Pequeno Medio*/
 );
 
 create table cliente(
     CPF_C integer primary key,
-    Nome varchar(128) NOT NULL,
-	Endereco varchar(128) NOT NULL,
-    Telefone char(12) NOT NULL
+    Nome varchar(128),
+	Endereco varchar(128),
+    Telefone char(12)
 );
 
 create table Animal(
 	Animal_ID integer primary key,
     CPF_C integer NOT NULL,
     Race_ID integer,
-    Nome varchar(128) NOT NULL,
+    Nome varchar(128),
     obs varchar(256),
     foreign key(CPF_C) references Cliente(CPF_C),
     foreign key(Race_ID) references Raca(Race_ID)  
